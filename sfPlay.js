e9080d3e36ec35129b99e6ae76057b67abba79a7
Merritt Leidy <merritt.leidy@gmail.com>
2025-03-21 14:31:55 -0400

    3/21/25

--- sfPlay.js
+++ sfPlay.js
@@ -7,8 +7,20 @@ masterGain.gain.value = 1.0; // Set the master volume to full (1.0)
 
 // Connect the master gain node to the audio output (speakers or headphones)
 masterGain.connect(soundCtx.destination);
+let source;
+const loadPlayAudio = async function () {
+  const file = await fetch("owls.wav");
+  const arrayBuffer = await file.arrayBuffer();
+  const audioBuffer = await soundCtx.decodeAudioData(arrayBuffer);
+  source = soundCtx.createBufferSource();
+  source.buffer = audioBuffer;
+  source.connect(masterGain);
+  source.start();
+};
 
-
+const stopAudio = function () {
+  source.stop();
+};
 
 // Add a click event listener to the "start" button
 // When clicked, it runs loadPlayAudio() to fetch, decode, and play the sound
